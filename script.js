1ced4c44aa445f60ece013b53d8ca6d3e7026893
palomageografa <113318168+palomageografa@users.noreply.github.com>
2024-11-14 15:07:23 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Manejador de formulario de contacto
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('¡Gracias por tu mensaje! Te responderé a la brevedad.');
+});
